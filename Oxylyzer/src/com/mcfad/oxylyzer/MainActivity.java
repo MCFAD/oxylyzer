4139ee95c98d5d16691f102f66e7162e9b1dff04
Michaeljczhang <zhang.jian.xi@hotmail.com>
2014-03-03 14:59:52 -0800

    delete redundant data

--- Oxylyzer/src/com/mcfad/oxylyzer/MainActivity.java
+++ Oxylyzer/src/com/mcfad/oxylyzer/MainActivity.java
@@ -1,15 +1,40 @@
 package com.mcfad.oxylyzer;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends Activity {
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// init example series data
+		GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
+		      new GraphViewData(1, 2.0d)
+		      , new GraphViewData(2, 1.5d)
+		      , new GraphViewData(3, 2.5d)
+		      , new GraphViewData(4, 1.0d)
+		});
+		 
+		GraphView graphView = new LineGraphView(
+		      this // context
+		      , "GraphViewDemo" // heading
+		);
+		graphView.addSeries(exampleSeries); // data
+		 
+		LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);
+		layout.addView(graphView);
 	}
 
 	@Override
