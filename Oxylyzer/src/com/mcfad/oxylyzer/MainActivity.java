26e186ef7b3dacfdb252b34655b679515560e144
Chris Tolliday <ctolliday@gmail.com>
2014-03-03 15:01:57 -0800

    simple realtime graph

--- Oxylyzer/src/com/mcfad/oxylyzer/MainActivity.java
+++ Oxylyzer/src/com/mcfad/oxylyzer/MainActivity.java
@@ -6,35 +6,46 @@ import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.LineGraphView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.LinearLayout;
 
 
 public class MainActivity extends Activity {
-	
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		// init example series data
-		GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
-		      new GraphViewData(1, 2.0d)
-		      , new GraphViewData(2, 1.5d)
-		      , new GraphViewData(3, 2.5d)
-		      , new GraphViewData(4, 1.0d)
-		});
-		 
-		GraphView graphView = new LineGraphView(
-		      this // context
-		      , "GraphViewDemo" // heading
-		);
+		final GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {});
+
+		GraphView graphView = new LineGraphView(this, "GraphViewDemo");
 		graphView.addSeries(exampleSeries); // data
-		 
+		graphView.setScrollable(true);
+		graphView.setViewPort(0, 10);
+		graphView.setManualYAxis(true);
+		graphView.setManualYAxisBounds(1, 0);
+
 		LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);
 		layout.addView(graphView);
+
+
+		final Handler handler = new Handler(Looper.getMainLooper());
+		Runnable graphUpdate = new Runnable() {
+			int x = 0;
+			@Override
+			public void run() {
+				x += 1d;
+				exampleSeries.appendData(new GraphViewData(x, Math.random()), true, 100);
+				handler.postDelayed(this, 200);
+			}
+		};
+		handler.postDelayed(graphUpdate, 1000);
 	}
 
 	@Override
