feb95a851499f9137de6c61aca25a166306ea525
Chris Tolliday <ctolliday@gmail.com>
2014-03-08 15:13:48 -0800

    Fixed tabs

--- Oxylyzer/src/com/mcfad/oxylyzer/MainActivity.java
+++ Oxylyzer/src/com/mcfad/oxylyzer/MainActivity.java
@@ -1,51 +1,88 @@
 package com.mcfad.oxylyzer;
 
+import java.util.Locale;
+
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.LineGraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
-public class MainActivity extends Activity {
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a
+	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+	 * will keep every loaded fragment in memory. If this becomes too memory
+	 * intensive, it may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	SectionsPagerAdapter mSectionsPagerAdapter;
 
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	ViewPager mViewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// init example series data
-		final GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {});
+		// Set up the action bar.
+		final ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		GraphView graphView = new LineGraphView(this, "GraphViewDemo");
-		graphView.addSeries(exampleSeries); // data
-		graphView.setScrollable(true);
-		graphView.setViewPort(0, 10);
-		graphView.setManualYAxis(true);
-		graphView.setManualYAxisBounds(1, 0);
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the app.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(
+				getSupportFragmentManager());
 
-		LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);
-		layout.addView(graphView);
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
-		final Handler handler = new Handler(Looper.getMainLooper());
-		Runnable graphUpdate = new Runnable() {
-			int x = 0;
+		// When swiping between different sections, select the corresponding
+		// tab. We can also use ActionBar.Tab#select() to do this if we have
+		// a reference to the Tab.
+		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
 			@Override
-			public void run() {
-				x += 1d;
-				exampleSeries.appendData(new GraphViewData(x, Math.random()), true, 100);
-				handler.postDelayed(this, 200);
+			public void onPageSelected(int position) {
+				actionBar.setSelectedNavigationItem(position);
 			}
-		};
-		handler.postDelayed(graphUpdate, 1000);
+		});
+
+		// For each of the sections in the app, add a tab to the action bar.
+		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+			// Create a tab with text corresponding to the page title defined by
+			// the adapter. Also specify this Activity object, which implements
+			// the TabListener interface, as the callback (listener) for when
+			// this tab is selected.
+			actionBar.addTab(actionBar.newTab()
+					.setText(mSectionsPagerAdapter.getPageTitle(i))
+					.setTabListener(this));
+		}
 	}
 
 	@Override
@@ -53,6 +90,113 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+		// When the given tab is selected, switch to the corresponding page in
+		// the ViewPager.
+		mViewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+	/**
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the sections/tabs/pages.
+	 */
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			// getItem is called to instantiate the fragment for the given page.
+			// Return a DummySectionFragment (defined as a static inner class
+			// below) with the page number as its lone argument.
+			Fragment fragment = new DummySectionFragment();
+			Bundle args = new Bundle();
+			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+			fragment.setArguments(args);
+			return fragment;
+		}
+
+		@Override
+		public int getCount() {
+			// Show 3 total pages.
+			return 2;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			Locale l = Locale.getDefault();
+			switch (position) {
+			case 0:
+				return "Realtime";
+			case 1:
+				return "History";
+			}
+			return null;
+		}
+	}
+
+	/**
+	 * A dummy fragment representing a section of the app, but that simply
+	 * displays dummy text.
+	 */
+	public static class DummySectionFragment extends Fragment {
+		/**
+		 * The fragment argument representing the section number for this
+		 * fragment.
+		 */
+		public static final String ARG_SECTION_NUMBER = "section_number";
+
+		public DummySectionFragment() {}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main2_dummy, container, false);
+			setupGraph(rootView);
+			return rootView;
+		}
+
+		public void setupGraph(View rootView){
+			// init example series data
+			final GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {});
+
+			GraphView graphView = new LineGraphView(this.getActivity(), "GraphViewDemo");
+			graphView.addSeries(exampleSeries); // data
+			graphView.setScrollable(true);
+			graphView.setViewPort(0, 10);
+			graphView.setManualYAxis(true);
+			graphView.setManualYAxisBounds(1, 0);
+
+			LinearLayout layout = (LinearLayout) rootView.findViewById(R.id.graph1);
+			layout.addView(graphView);
+
+
+			final Handler handler = new Handler(Looper.getMainLooper());
+			Runnable graphUpdate = new Runnable() {
+				int x = 0;
+				@Override
+				public void run() {
+					x += 1d;
+					exampleSeries.appendData(new GraphViewData(x, Math.random()), true, 100);
+					handler.postDelayed(this, 200);
+				}
+			};
+			handler.postDelayed(graphUpdate, 1000);
+		}
 	}
 
 }
