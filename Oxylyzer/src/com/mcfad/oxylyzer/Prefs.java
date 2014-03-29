4f0e4ed77079e7ce6807d2cf3dc737d089bcb0d6
Michaeljczhang <zhang.jian.xi@hotmail.com>
2014-03-29 01:54:05 -0700

    added Setting page : Prefs.java and prefs.xml

---
+++ Oxylyzer/src/com/mcfad/oxylyzer/Prefs.java
@@ -0,0 +1,16 @@
+package com.mcfad.oxylyzer;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Prefs extends PreferenceActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.prefs);
+	}
+	
+
+}
