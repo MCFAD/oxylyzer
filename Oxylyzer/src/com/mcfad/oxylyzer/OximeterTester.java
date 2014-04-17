80a8f1ee0933042c74c5f5132a3d4391a944ef67
Michaeljczhang <zhang.jian.xi@hotmail.com>
2014-04-16 22:50:23 -0700

    push

--- Oxylyzer/src/com/mcfad/oxylyzer/OximeterTester.java
+++ Oxylyzer/src/com/mcfad/oxylyzer/OximeterTester.java
@@ -2,7 +2,11 @@ package com.mcfad.oxylyzer;
 
 import java.util.Date;
 
+import com.mcfad.oxylyzer.db.OxContentProvider;
+
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v4.content.LocalBroadcastManager;
@@ -10,24 +14,38 @@ import android.support.v4.content.LocalBroadcastManager;
 public class OximeterTester {
 
 	LocalBroadcastManager lBroadMan;
+	Context context = null;
 	public OximeterTester(final MainActivity activity){
+		context = activity;
 		lBroadMan = LocalBroadcastManager.getInstance(activity);
 		final Handler handler = new Handler(Looper.getMainLooper());
 		Runnable graphUpdate = new Runnable() {
 			int x = 0;
 			long time = new Date().getTime();
+			
 			@Override
 			public void run() {
+				if(recordingUri==null){
+					recordingUri = OxContentProvider.startNewRecording(context,time);
+				}
 				postData(time);
 				time += 1000;
-				handler.postDelayed(this, 10);
+				
 				x += 1;
+				
+				if(x > 1000 && recordingUri!=null){
+					OxContentProvider.endRecording(context, recordingUri,time);
+				}
+				else
+					handler.postDelayed(this, 10);
 			}
 		};
 		handler.post(graphUpdate);
 	}
 	double spo2 = 97;
 	double bpm = 68;
+	Uri recordingUri = null;
+
 	public void postData(long time){
 		//int spO2 = (int) (80+20*Math.random());
 		//int bpm = (int) (80+20*Math.random());
@@ -56,7 +74,9 @@ public class OximeterTester {
 		intent.putExtra("spo2", (int)Math.round(spo2));
 		intent.putExtra("bpm", (int)Math.round(bpm));
 
-
+		
+		OxContentProvider.postDatapoint(context, recordingUri, time, (int)Math.round(spo2), (int)Math.round(bpm));
+		
 		lBroadMan.sendBroadcast(intent);
 	}
 }
--- Oxylyzer/src/com/mcfad/oxylyzer/view/HistoryOxGraph.java
+++ Oxylyzer/src/com/mcfad/oxylyzer/view/HistoryOxGraph.java
@@ -41,7 +41,7 @@ public class HistoryOxGraph extends OxGraph {
 		Cursor dataCursor = recording.queryDatapoints(context);
 		int apneaStartTime = Integer.MAX_VALUE;
 		int baseLine = 98;//Set this to baseLine If baseline is available
-		int apneaClassificationTime = 1;  //change this to a lower value if you want to see a premature apnea event
+		int apneaClassificationTime = 5;  //change this to a lower value if you want to see a premature apnea event
 		int previousSPO2 = 0;
 		DataPoint previousDataPoint = null;
 		while(dataCursor.moveToNext()){
